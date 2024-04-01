275522f8bdfb13aae0f69ca898bbc09819569a31
Cristofer Gonzalez <criss.gonzalez97@gmail.com>
2024-04-01 02:43:37 -0300

    02-pokeApi_get

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,14 @@ export const App = () => {
   const getPokemons = async () => {
     try {
       const response = await axios.get(
-        "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0"
+        "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0"
       );
-      setPokemons(response.data.results);
+      const pokemonData = response.data.results.map((pokemon) => {
+        const urlParts = pokemon.url.split("/");
+        const id = urlParts[urlParts.length - 2];
+        return { ...pokemon, id };
+      });
+      setPokemons(pokemonData);
     } catch (error) {
       console.log(error);
     }
@@ -17,11 +22,32 @@ export const App = () => {
     getPokemons();
   }, []);
   return (
-    <>
-      <h1>Pokemons Api</h1>
-      {pokemons.map((pokemon) => (
-        <p key={pokemon.name}>{pokemon.name}</p>
-      ))}
-    </>
+    <main className="flex flex-col min-h-screen justify-center items-center w-full ">
+      <div
+        className="flex flex-grid gap-4
+      m-4"
+      >
+        <h1 className="text-4xl font-bold text-slate-800">Pokemons API</h1>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Pokebola-pokeball-png-0.png/640px-Pokebola-pokeball-png-0.png"
+          alt="pokeball imagen"
+          className="w-10"
+        />
+      </div>
+      <ul className="grid grid-cols-4 gap-10">
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.id} className="bg-blue-700  p-2  rounded-lg">
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
+              alt={`${pokemon.name} sprite`}
+              className="w-[250px] h-[250px]"
+            />
+            <div className="bg-blue-600 rounded-lg border-blue">
+              <p className="text-white capitalize ">{pokemon.name}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </main>
   );
 };
